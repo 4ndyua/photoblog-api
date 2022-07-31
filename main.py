da1fce42e58a576c866af2c90095dc4a09cd7fbb
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-08-01 00:22:21 +0300

    auth and fixes

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from routers import user, post
+from fastapi.staticfiles import StaticFiles
+from auth import authentication
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(authentication.router)
 
 
 @app.get("/")
@@ -14,3 +17,5 @@ async def root():
     return {"message": "Hello World"}
 
 models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
--- routers/post.py
+++ routers/post.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File
 from db.db_post import create_new_post, get_all, delete_post
 from db.database import get_db
 from routers.schemas import UserAuth, PostBase, PostDisplay
+from auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix='/post',
@@ -15,8 +16,7 @@ image_url_types = ['absolute', 'relative']
 
 
 @router.post('', response_model=PostDisplay)
-def create(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user),
-           get_current_user=None):
+def create(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
     if request.image_url_type not in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                             detail="Parameter image_url_type can only take values 'absolute' or 'relative'.")
