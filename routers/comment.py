9d0ddda46fa27a1f6ffa833417ae7e1b0db287a2
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-08-01 12:27:33 +0300

    fixes

--- routers/comment.py
+++ routers/comment.py
@@ -5,17 +5,18 @@ from db.database import get_db
 from db import db_comment
 from auth.oauth2 import get_current_user
 
-router = APIRouter(
-    prefix='/comment',
-    tags=['comment']
-)
+router = APIRouter(prefix="/comment", tags=["comment"])
 
 
-@router.get('/all/{post_id}')
+@router.get("/all/{post_id}")
 def comments(post_id: int, db: Session = Depends(get_db)):
     return db_comment.get_all(db, post_id)
 
 
-@router.post('')
-def create(request: CommentBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+@router.post("")
+def create(
+    request: CommentBase,
+    db: Session = Depends(get_db),
+    current_user: UserAuth = Depends(get_current_user),
+):
     return db_comment.create(db, request)
