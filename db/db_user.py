fafac1b1ca82d5729ac4508fd99c5542f815d83f
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-08-01 11:08:40 +0300

    user functionality, fixes

--- db/db_user.py
+++ db/db_user.py
@@ -25,3 +25,31 @@ def get_user_by_username(db: Session, username: str):
             detail=f"User with username {username} not found",
         )
     return user
+
+
+def update_user(db: Session, id: int, request: UserBase):
+    user = db.query(DbUser).filter(DbUser.id == id)
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found"
+        )
+    user.update(
+        {
+            DbUser.username: request.username,
+            DbUser.email: request.email,
+            DbUser.password: get_password_hash(request.password),
+        }
+    )
+    db.commit()
+    return "OK"
+
+
+def delete_user(db: Session, id: int):
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found"
+        )
+    db.delete(user)
+    db.commit()
+    return "ok"
